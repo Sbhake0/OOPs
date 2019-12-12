7e3f9c3b2b1472705a1c336c58338d1d10abee90
Sbhake0 <40068019+Sbhake0@users.noreply.github.com>
2019-12-12 08:47:24 -0800

    Commit n Push

---
+++ oop/Address.java
@@ -0,0 +1,67 @@
+package com.scp.java.oop;
+
+public class Address {
+
+		private int aid;
+		private String city;
+		private int pincode;
+		
+		
+	
+	public Address(int aid, String city, int pincode) {
+			super();
+			this.aid = aid;
+			this.city = city;
+			this.pincode = pincode;
+		}
+
+
+
+	public int getAid() {
+			return aid;
+		}
+
+
+
+		public void setAid(int aid) {
+			this.aid = aid;
+		}
+
+
+
+		public String getCity() {
+			return city;
+		}
+
+
+
+		public void setCity(String city) {
+			this.city = city;
+		}
+
+
+
+		public int getPincode() {
+			return pincode;
+		}
+
+
+
+		public void setPincode(int pincode) {
+			this.pincode = pincode;
+		}
+
+
+
+	@Override
+		public String toString() {
+			return "\n [aid=" + aid + ", city=" + city + ", pincode=" + pincode + "]";
+		}
+
+
+
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ oop/StudAppConstants.java
@@ -0,0 +1,15 @@
+package com.scp.java.oop;
+
+public interface StudAppConstants {
+
+	public String DUPLICATE_STUDENT = "Duplicate Student exist with given Id....cannot add";
+	public String INVALID_STUDENT_ID = "Invalid Student Id";
+	public String INVALID_STUDENT = "Invalid Student...Cannot Add/Update/Delete...";
+	public String NOT_A_STUDENT = "Not a student....cannot add/update";
+	public String STUDENT_SUCCESS = "Student Added Successfully....!";
+	public String STUDENT_CAPACITY = "Capacity exceeded....!";
+	public String INVALID_STUDENT_CAPACITY ="Invalid number...cannot start the application.";
+	public String INVALID_STUDENT_ACTIVITY ="Cannot add..due to some internal activity upgrade...";
+	
+	
+}
